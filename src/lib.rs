1c647c10fa2e7b3b92a8e0cb1b6940aff5515257
willemml <willem@leit.so>
2025-02-09 12:01:13 -0700

    Adds hepler function to parse from Read

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,27 @@
 #![feature(if_let_guard)]
 
+use std::io::{BufReader, Read};
+
+use xml::{EventReader, ParserConfig};
+
 pub mod data_types;
 pub mod error;
 pub mod parser;
+
+pub fn parse_buffer<R: Read>(
+    from: R,
+) -> Result<Result<data_types::XDFElement, error::Error>, std::io::Error> {
+    let file = BufReader::new(from); // Buffering is important for performance
+
+    let mut parser = EventReader::new_with_config(
+        file,
+        ParserConfig::new()
+            .ignore_comments(true)
+            .trim_whitespace(true)
+            .whitespace_to_characters(true)
+            .cdata_to_characters(true)
+            .coalesce_characters(true),
+    );
+
+    Ok(data_types::XDFElement::from_xml(&mut parser))
+}
--- tests/8E0909518AK.rs
+++ tests/8E0909518AK.rs
@@ -2,28 +2,11 @@
 #![feature(decl_macro)]
 
 use std::fs::File;
-use std::io::BufReader;
-
-use xml::reader::EventReader;
-use xml::ParserConfig;
-
-use xdftuneparser::data_types::*;
+use xdftuneparser::parse_buffer;
 
 /// needs to be broken out/improved/verified.
 #[test]
 fn parse_amb_xdf() {
     let file = File::open("tests/8E0909518AK_368072_NEF_STG_1v7.xdf").unwrap();
-    let file: BufReader<File> = BufReader::new(file); // Buffering is important for performance
-
-    let mut parser = EventReader::new_with_config(
-        file,
-        ParserConfig::new()
-            .ignore_comments(true)
-            .trim_whitespace(true)
-            .whitespace_to_characters(true)
-            .cdata_to_characters(true)
-            .coalesce_characters(true),
-    );
-
-    let _ = XDFElement::from_xml(&mut parser).unwrap();
+    parse_buffer(file).unwrap().unwrap();
 }
